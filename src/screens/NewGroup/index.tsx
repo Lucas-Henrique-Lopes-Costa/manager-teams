01165829aa4486f7ee34e7594ed4f2cb7fa98241
Lucas Henrique Lopes Costa <lucass20586@gmail.com>
2023-03-28 19:47:53 -0300

    input

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -3,6 +3,7 @@ import { Container, Content, Icon } from "./styles";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
+import { Input } from "@components/Input";
 
 export function NewGroup() {
   return (
@@ -17,8 +18,13 @@ export function NewGroup() {
           subtitle="Crie sua turma para adicionar as pessoas"
         />
 
+        <Input 
+          placeholder="Nome da Turma"
+        />
+
         <Button 
           title="Criar"
+          style={{ marginTop: 20 }}
         />
       </Content>
     </Container>
