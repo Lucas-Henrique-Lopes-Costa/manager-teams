07589d371f549aee234f8d76c3057c36fc268d78
Lucas Henrique Lopes Costa <lucass20586@gmail.com>
2023-03-28 23:16:54 -0300

    criando filtro

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,18 @@
+import { FlatList } from "react-native";
+import { useState } from "react";
+
 import { Input } from "@components/Input";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 
-import { Container, Form } from "./styles";
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
+import { Filter } from "@components/Filter";
 
 export function Players() {
+  const [team, setTeam] = useState("Time B");
+  const [players, setPlayers] = useState(["Lucas", "Marcos"])
+
   return (
     <Container>
       <Header showBackButton />
@@ -19,6 +26,25 @@ export function Players() {
         <Input placeholder="Nome da pessoa" autoCorrect={false} />
         <ButtonIcon icon="add" />
       </Form>
+
+      <HeaderList>
+        <FlatList
+          data={["Time A", "Time B"]}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+
+        <NumberOfPlayers>
+            {players.length}
+        </NumberOfPlayers>
+      </HeaderList>
     </Container>
   );
 }
